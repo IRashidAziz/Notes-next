71756c0bddab1d69f2a2a04eb49be3f3a091a415
IRashidAziz <hm.davidhussain@gmail.com>
2025-11-12 19:39:40 +0500

    Added Fade Animation

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+"use client";
 import TitleProvider from "@/components/providers/TitleProvider";
 import Header from "@/components/layout/Header";
 import Button from "@/components/ui/Button";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -9,21 +10,27 @@ export default function Home() {
       <TitleProvider>FlashNotes</TitleProvider>
       <div className="home">
         <Header />
-        <div className="hero">
-          Manage Your Notes, Tasks and Projects{" "}
-          <span className="fancy">Efficiently</span>
-        </div>
-        <div className="hero-description">
-          Effortlessly organize your tasks, streamline your workflows, and
-          enhance collaboration with our all-in-one solution. Whether
-          you&apos;re managing personal projects, working solo, or teaming up
-          with colleagues, our platform helps you stay focused, productive, and
-          on top of your game.
-        </div>
-        <div className="buttons">
-          <Button variant="white">Learn More</Button>
-          <Button variant="black">Get Started</Button>
-        </div>
+        <motion.main
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.2 }}
+        >
+          <div className="hero">
+            Manage Your Notes, Tasks and Projects{" "}
+            <span className="fancy">Efficiently</span>
+          </div>
+          <div className="hero-description">
+            Effortlessly organize your tasks, streamline your workflows, and
+            enhance collaboration with our all-in-one solution. Whether
+            you&apos;re managing personal projects, working solo, or teaming up
+            with colleagues, our platform helps you stay focused, productive,
+            and on top of your game.
+          </div>
+          <div className="buttons">
+            <Button variant="white">Learn More</Button>
+            <Button variant="black">Get Started</Button>
+          </div>
+        </motion.main>
       </div>
     </>
   );
